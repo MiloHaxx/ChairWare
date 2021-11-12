4ca0f8589ea7ff1b5e91123dbe652fc6d6870ecd
MiloHaxx <94107299+MiloHaxx@users.noreply.github.com>
2021-11-12 12:38:38 +0100

    Reworked loader

--- loader.lua
+++ loader.lua
@@ -1,3 +1,9 @@
-if game.PlaceId == 4580204640 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/MiloHaxx/ChairWare/main/SurviveTheKiller.lua", true))()
+local games = {
+
+    ["SurviveTheKiller"] = 4580204640
+
+}
+
+for i,v in pairs(games) do
+    if game.PlaceId == v then loadstring(game:HttpGet("https://raw.githubusercontent.com/MiloHaxx/ChairWare/main/" .. i .. ".lua"))()
 end
