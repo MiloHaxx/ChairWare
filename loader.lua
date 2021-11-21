71785f786246bcf765a12de7fc2aa69bbf4380a6
MiloHaxx <94107299+MiloHaxx@users.noreply.github.com>
2021-11-21 15:33:49 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,7 @@ local games = {
 }
 
 for i,v in pairs(games) do
-    if game.PlaceId == v then print("Supported!"); loadstring(game:HttpGet("https://raw.githubusercontent.com/MiloHaxx/ChairWare/main/" .. i .. ".lua"))() end
+    if game.PlaceId == v then print("Supported!"); loadstring(game:HttpGet("https://raw.githubusercontent.com/MiloHaxx/ChairWare/main/Games" .. i .. ".lua"))() end
 end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MiloHaxx/ChairWare/main/dcJoin.lua"))()
